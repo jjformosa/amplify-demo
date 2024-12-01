fa31446686be3037138bf0f4d75165d4bf54b5c2
jjformosa <jjformosa1220@gmail.com>
2024-12-01 15:31:05 +0800

    fix:auto switch to http on prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import fs from 'fs';
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@amplify': path.resolve(__dirname, './amplify'),
-      '@root': path.resolve(__dirname)
+export default defineConfig(({ command, mode /*, isSsrBuild, isPreview*/ }) => {
+  console.log(`command: ${command}, mode: ${mode}`);
+  const useHttps = command !== 'build';
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@src': path.resolve(__dirname, './src'),
+        '@amplify': path.resolve(__dirname, './amplify'),
+        '@root': path.resolve(__dirname)
+      },
     },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './.cert/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './.cert/cert.pem'))
-    },
-    port: 5173,
-    host: '0.0.0.0'
+    server: {
+      https: useHttps ? {
+        key: fs.readFileSync(path.resolve(__dirname, './.cert/key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, './.cert/cert.pem'))
+      } : undefined,
+      port: 5173,
+      host: '0.0.0.0'
+    }
   }
 })
