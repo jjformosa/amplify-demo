d3224dbfbf5d318b385444066eaee2cddc95ab14
jjformosa <jjformosa1220@gmail.com>
2024-12-01 14:56:58 +0800

    feat:[Day4] add aws sdk

--- amplify/functions/auth/verifyAuthChallengeResponse/resource.ts
+++ amplify/functions/auth/verifyAuthChallengeResponse/resource.ts
@@ -4,8 +4,8 @@ const verifyAuthChallengeResponse = defineFunction({
   name: 'verify-auth-challenge-response',
   environment: {
     LINE_IDENTITY_POOL_ID: 'us-east-1:35e7edb4-7bae-4c9e-a3eb-4a094021b954',
-    LIFF_CLIENT_ID: secret('LIFF_CLIENT_ID'),
-    LIFF_CLIENT_SECRET: secret('LIFF_CLIENT_SECRET')
+    LINE_ID: secret('LINE_ID'),
+    LINE_SECRET: secret('LIFF_CLIENT_SECRET')
   }
 })
 
