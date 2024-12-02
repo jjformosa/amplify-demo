078e422decf3f6db4377ab784483ecf4f6858bd9
jjformosa <jjformosa1220@gmail.com>
2024-12-03 00:14:40 +0800

    fix:[WIP][Day5]try to fix defineAuthChallenge

--- amplify/functions/auth/defineAuthChallenge/handler.ts
+++ amplify/functions/auth/defineAuthChallenge/handler.ts
@@ -7,8 +7,9 @@ const cognitClient = new AWS.CognitoIdentityServiceProvider()
 export const handler: DefineAuthChallengeTriggerHandler = async (event) => {
   event.response.issueTokens = false
   event.response.failAuthentication = false
-  console.log('event', event.request.userNotFound)
   const { email, picture, name } = event.request.clientMetadata ?? {}
+  console.log('event', event.request.userNotFound)
+  console.log('event', email, picture, name)
   const [challengeResponse] = event.request.session
   if (challengeResponse?.challengeName === 'CUSTOM_CHALLENGE') {
     // 代表本次請求來自某一則session的挑戰回應
@@ -19,12 +20,12 @@ export const handler: DefineAuthChallengeTriggerHandler = async (event) => {
   } else {
     console.log('userAttributes')
     printEachOfStringMap(event.request.userAttributes)
-    // const email = event.request.userAttributes.email
+    const e = event.request.userAttributes.email
     // 代表本次請求來自某個Amplify Client的首次請求
     const filterParams = {
       UserPoolId: event.userPoolId,
       AttributesToGet: ['email'],
-      Filter: `email = "${email}"`
+      Filter: `email = "${e}"`
     }
     // 確認email已經存在於userpool
     const listUsersResponse = await cognitClient.listUsers(filterParams).promise()
@@ -34,11 +35,11 @@ export const handler: DefineAuthChallengeTriggerHandler = async (event) => {
       // event.response.failAuthentication = true
       const createUserParams = {
         UserPoolId: event.userPoolId,
-        Username: email,
+        Username: e,
         UserAttributes: [
           {
             Name: 'email',
-            Value: email
+            Value: e
           }
         ]
       }
