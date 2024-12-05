ebd07fe04f6edbaaca1a6a8441573a447f3427ca
jjformosa <jjformosa1220@gmail.com>
2024-12-05 10:16:42 +0800

    feat:[WIP][Day8]test file permission

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { storage, secondBucket } from './storage/resource';
+import { secondBucket } from './storage/resource';
 
 defineBackend({
   auth,
   data,
-  storage,
   secondBucket
 });
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,24 +1,24 @@
 import { defineStorage } from '@aws-amplify/backend';
 
-export const storage = defineStorage({
-  name: 'amplifyDemoDrive',
-  access: (allow) => ({
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete']),
-    ],
-    'personal/{entity_id}/*':[
-        // only owner can read, write, delete
-        allow.entity('identity').to(['read', 'write', 'delete']),
-        allow.groups(['admin']).to(['read', 'delete']),
-        // allow.authenticated.to(['read']), Specify user
-    ],
-    'public/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete']),
-        allow.groups(['admin']).to(['read', 'delete']),
-        allow.authenticated.to(['read'])
-    ]
-  })
-});
+// export const storage = defineStorage({
+//   name: 'amplifyDemoDrive',
+//   access: (allow) => ({
+//     'private/{entity_id}/*': [
+//       allow.entity('identity').to(['read', 'write', 'delete']),
+//     ],
+//     'personal/{entity_id}/*':[
+//         // only owner can read, write, delete
+//         allow.entity('identity').to(['read', 'write', 'delete']),
+//         allow.groups(['admin']).to(['read', 'delete']),
+//         // allow.authenticated.to(['read']), Specify user
+//     ],
+//     'public/{entity_id}/*': [
+//         allow.entity('identity').to(['read', 'write', 'delete']),
+//         allow.groups(['admin']).to(['read', 'delete']),
+//         allow.authenticated.to(['read'])
+//     ]
+//   })
+// });
 
 export const secondBucket = defineStorage({
   name: 'secondBucket',
